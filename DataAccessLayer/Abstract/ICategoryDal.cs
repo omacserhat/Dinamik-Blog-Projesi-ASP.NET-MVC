7ba621a0cdfcf00ada8dc6f3d3ae4f9321621155
Serhat OMAÇ <omacserhatt@gmail.com>
2022-10-18 14:20:08 +0300

    Repository, GenericRepository, Business Katmanı Service ve Manager yazıldı, Entityframework Repository yazıldı

--- DataAccessLayer/Abstract/ICategoryDal.cs
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -7,12 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface ICategoryDal
+    public interface ICategoryDal:IGenericDal<Category>
     {
-        List<Category> ListAllCategory();
-        void AddCategory(Category category);
-        void DeleteCategory(Category category);
-        void UpdateCategory(Category category);
-        Category GetById(int id);
     }
 }
