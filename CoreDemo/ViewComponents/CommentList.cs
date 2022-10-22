bf1fe585b6e07c2f37f8d0fe8cf03d1f3848fe45
Serhat OMAÇ <omacserhatt@gmail.com>
2022-10-22 18:49:51 +0300

    Kategori ve yorumlar component haline getirilerek kodlar daha temiz yazıldı.

---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,32 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentValues = new List<UserComment>
+            {
+                new UserComment
+                {
+                 ID = 1,
+                 UserName = "Serhat"
+                },
+                 new UserComment
+                {
+                 ID = 2,
+                 UserName = "Cihat"
+                },
+                  new UserComment
+                {
+                 ID = 3,
+                 UserName = "Ahmet"
+                },
+            };
+            return View(commentValues);
+        }
+    }
+}
