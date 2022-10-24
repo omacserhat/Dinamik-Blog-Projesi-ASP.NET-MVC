6a284dce3f3a7dee279c87737e28fcdc7fd7b4f8
Serhat OMAÇ <omacserhatt@gmail.com>
2022-10-24 20:29:09 +0300

    Blog sayfası düzenlemeleri

---
+++ DataAccessLayer/Migrations/20221024170032_mig_writer_blog_relation.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_writer_blog_relation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WriterID1",
+                table: "Writers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WriterID",
+                table: "Blogs",
+                type: "int",
+                nullable: true,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Writers_WriterID1",
+                table: "Writers",
+                column: "WriterID1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_WriterID",
+                table: "Blogs",
+                column: "WriterID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Writers_WriterID",
+                table: "Blogs",
+                column: "WriterID",
+                principalTable: "Writers",
+                principalColumn: "WriterID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Writers_Writers_WriterID1",
+                table: "Writers",
+                column: "WriterID1",
+                principalTable: "Writers",
+                principalColumn: "WriterID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Writers_WriterID",
+                table: "Blogs");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Writers_Writers_WriterID1",
+                table: "Writers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Writers_WriterID1",
+                table: "Writers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Blogs_WriterID",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "WriterID1",
+                table: "Writers");
+
+            migrationBuilder.DropColumn(
+                name: "WriterID",
+                table: "Blogs");
+        }
+    }
+}
